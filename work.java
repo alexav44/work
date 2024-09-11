068816e38d59ac71b48f2eaf2127cb92e8471c68
alexav44 <alexav44@nycstudents.net>
2024-09-11 03:19:08 +0000

    first commit

---
+++ work.java
@@ -0,0 +1,20 @@
+public class work
+{ 
+    public static void main(String[] args)
+    {
+        String animal = "cat";
+        double weight = 13.0;
+        int weightGained = 4;
+        weight = weight + weightGained;
+        final double goal = 11;
+        if(weight > 12){
+            System.out.println("Your " + animal + " is overweight, and currently weighs " + weight + " lbs");
+            System.out.print("Try to aim for " + goal + "lbs");
+        }
+        else{
+            System.out.println("Your" + animal + "is purrfect, and currently weighs" + weight);
+        }
+
+    }
+
+}
